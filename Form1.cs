251d3d6972391d159aea3df244b46fa97ecc7ea8
Daniel-x2 <foconoaltar123@gmail.com>
2025-04-02 13:38:44 -0300

    fazendo algumas correçoes

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,8 @@
 using static System.Console;
 using System.Collections;
+using System;
+using System.Security.Permissions;
+using System.Data;
 namespace WinFormsApp2
 {
     public partial class Form1 : Form
@@ -27,6 +30,7 @@ namespace WinFormsApp2
             aspas.Click += label1_Click_1;
             ac.Click += label1_Click_1;
             delete.Click += label1_Click_1;
+            resultado.Click += label1_Click_1;
         }
 
         private void Menos_Click(object? sender, EventArgs e )
@@ -37,6 +41,7 @@ namespace WinFormsApp2
         private void label1_Click_1(object sender, EventArgs e)
         {
             label1.Text = n1;
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -113,6 +118,26 @@ namespace WinFormsApp2
 
         private void resultado_Click(object sender, EventArgs e)
         {
+            string copia=null;
+            int veri = copia.Length;
+            foreach (var c in n1 )
+            {
+                if (c == 'x')
+                {
+                    copia += "*";
+                }
+                else
+                {   
+                    
+                    copia += c;
+                }
+            }
+
+            
+            n1 = copia;
+            DataTable datatable = new DataTable();
+            var n2=datatable.Compute(n1, string.Empty);
+            n1 = n2.ToString();
 
         }
 
@@ -138,6 +163,7 @@ namespace WinFormsApp2
 
         private void delete_Click(object sender, EventArgs e)
         {
+            //remove o ultimo elemento adicionado
            int n2=n1.Length-1;
             string n3=n1.Remove(n2);
             n1 = n3;
@@ -146,6 +172,8 @@ namespace WinFormsApp2
 
         private void virgula_Click(object sender, EventArgs e)
         {
+            //adiciona a virgula
+
             n1 += ",";
         }
     }
